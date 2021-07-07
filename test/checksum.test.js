e15d3d75d3cdfcb0e7887f9bd2b3bd6ebd711f7f
Bryan English <bryan@bryanenglish.com>
2021-07-07 10:56:35 -0400

    switch to sha256 for default

--- test/checksum.test.js
+++ test/checksum.test.js
@@ -5,18 +5,18 @@ var checksum = require('..')
 tap.on('end', process.exit)
 
 test('checksum', function (t) {
-  t.equal(checksum('dshaw'), '9b8cebc0421241d087f6ab7e815285af803de7e7', 'simple value')
-  t.equal(checksum('1234567890~!@#$%^&*()_+'), 'd55303d0a19432c9689c9ebf51cee51f453b93bd', 'more chars')
+  t.equal(checksum('dshaw'), '650a94547ad8ff00d5c5e3c8d54e8503d566b8ab0029dc289b5bb4294e39b92a', 'simple value')
+  t.equal(checksum('1234567890~!@#$%^&*()_+'), 'cbfd8eb072b509a43c8171f735864e09e7df0ebf538d8c756d27891069c78f47', 'more chars')
   t.end()
 })
 
 test('checksum.file', function (t) {
   t.plan(3)
   checksum.file('./fixtures/dshaw.txt', function (err, sum) {
-    t.equal(sum, '9b8cebc0421241d087f6ab7e815285af803de7e7', 'text file checksum')
+    t.equal(sum, '650a94547ad8ff00d5c5e3c8d54e8503d566b8ab0029dc289b5bb4294e39b92a', 'text file checksum')
   })
   checksum.file('./fixtures/1px.gif', function (err, sum) {
-    t.equal(sum, 'c65ed837d46f9122ab047c33d2f9e947786187b4', 'binary file checksum')
+    t.equal(sum, 'db7164038271366318dc041126f07aafe9bf671340966f4765948d0b07f6c5cc', 'binary file checksum')
   })
   checksum.file('./idontexist.text', function (err, sum) {
     t.equal(err.code, 'ENOENT', 'the stat error will be passed along')
@@ -25,18 +25,18 @@ test('checksum.file', function (t) {
 
 // Passing encoding
 test('checksum.base64', function (t) {
-  t.equal(checksum('dshaw', { encoding: 'base64' }), 'm4zrwEISQdCH9qt+gVKFr4A95+c=', 'simple value')
-  t.equal(checksum('1234567890~!@#$%^&*()_+', { encoding: 'base64' }), '1VMD0KGUMslonJ6/Uc7lH0U7k70=', 'more chars')
+  t.equal(checksum('dshaw', { encoding: 'base64' }), 'ZQqUVHrY/wDVxePI1U6FA9VmuKsAKdwom1u0KU45uSo=', 'simple value')
+  t.equal(checksum('1234567890~!@#$%^&*()_+', { encoding: 'base64' }), 'y/2OsHK1CaQ8gXH3NYZOCeffDr9TjYx1bSeJEGnHj0c=', 'more chars')
   t.end()
 })
 
 test('checksum.file.base64', function (t) {
   t.plan(3)
   checksum.file('./fixtures/dshaw.txt', { encoding: 'base64' }, function (err, sum) {
-    t.equal(sum, 'm4zrwEISQdCH9qt+gVKFr4A95+c=', 'text file checksum')
+    t.equal(sum, 'ZQqUVHrY/wDVxePI1U6FA9VmuKsAKdwom1u0KU45uSo=', 'binary file checksum')
   })
   checksum.file('./fixtures/1px.gif', { encoding: 'base64' }, function (err, sum) {
-    t.equal(sum, 'xl7YN9RvkSKrBHwz0vnpR3hhh7Q=', 'binary file checksum')
+    t.equal(sum, '23FkA4JxNmMY3AQRJvB6r+m/ZxNAlm9HZZSNCwf2xcw=', 'text file checksum')
   })
   checksum.file('./idontexist.text', { encoding: 'base64' }, function (err, sum) {
     t.equal(err.code, 'ENOENT', 'the stat error will be passed along')
